d4d5a620c1adbf3d2f2e87fbf507de901c35477e
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-03 19:16:18 +0530

    PROGRAM 4

---
+++ 04pyramid_pattern_with_space.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter now of rows: ";
+  cin>>n;
+  
+ for(int i=1;i<=n;i++){
+     
+     //for space
+     for(int j=1;j<=n-i;j++){
+        cout<<" ";
+     }
+      //for printing i no of stars in i th row
+     for(int k=1;k<=i;k++){
+         cout<<"* ";
+     }
+     
+      cout<<endl;
+ }
+    return 0;
+}
