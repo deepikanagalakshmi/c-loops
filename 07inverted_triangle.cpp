a54c6b9ca10e447b28111de55bbd48fb3a60dd31
Deepika Nagalakshmi <58824208+deepikanagalakshmi@users.noreply.github.com>
2020-07-05 12:49:08 +0530

    Inverted Triangle OR Pyramid Pattern

---
+++ 07inverted_triangle.cpp
@@ -0,0 +1,23 @@
+//inverted triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter n :";
+	cin >> n;
+	for (int i = n; i > 0; i--) {
+		
+		//space n-i ----> 5-5=0 5-4=1  5-3=2  5-2=3  5-1= 4 
+		for (int j = 1; j <= n - i; j++) {
+			cout << " ";
+		}
+		
+		//star
+		for (int k = 1; k <= (2 * i )- 1; k++) {
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
